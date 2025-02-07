4b191a7660cb8ef8d356c1807a8b6229530d287b
maria <150175935+mmmaryae@users.noreply.github.com>
2025-02-06 22:47:25 -0300

    adicionando projeto de portfólio

---
+++ js/revael.js
@@ -0,0 +1,115 @@
+
+window.revelar = ScrollReveal({ reset: true })
+
+//PROCEDIMENTOS
+
+revelar.reveal('.efeito-txt-topo1', {
+    duration: 500,  // Diminui o tempo do efeito
+    distance: '90px',
+    delay: 400,     // Reduz o tempo de espera
+    once: true,
+    reset: false
+})
+
+
+revelar.reveal('.efeito-txt-topo2',
+    {
+        duration: 500,
+        distance: '90px',
+        delay:500,
+        once: true,
+        reset: false
+    })
+
+revelar.reveal('.efeito-txt-topo3',
+    {
+        duration: 500,
+        distance: '90px',
+        delay: 600,
+        once: true,
+        reset: false
+    })
+
+revelar.reveal('.efeito-txt-topo4',
+    {
+        duration: 500,
+        distance: '90px',
+        delay: 600,
+        once: true,
+        reset: false
+    })
+
+revelar.reveal('.efeito-txt-topo5',
+    {
+        duration: 500,
+        distance: '90px',
+        delay: 600,
+        once: true,
+        reset: false
+    })
+
+
+
+    
+revelar.reveal('.boasvindas',
+    {
+        duration: 2000,
+        distance: '90px',
+        delay: 800,
+        once: true,
+        reset: false
+    })
+
+
+//outros hr lado
+
+revelar.reveal('.efeito-sobre',
+    {
+        duration: 1000,
+        distance: '90px',
+        delay: 400,
+        origin:'left',
+        once: true,
+        reset: false
+    })
+
+    revelar.reveal('.efeito-sobre2',
+        {
+            duration: 1000,
+            distance: '90px',
+            delay: 400,
+            origin:'left',
+            once: true,
+            reset: false
+        })
+
+        revelar.reveal('.efeito-sobre3',
+            {
+                duration: 1000,
+                distance: '90px',
+                delay: 400,
+                origin:'left',
+                once: true,
+                reset: false
+            })
+
+
+            //conteudo sobe 
+            revelar.reveal('#efeito-sobe1',
+                {
+                    duration: 500,
+                    distance: '90px',
+                    delay: 600,
+                    once: true,
+                    reset: false
+                })
+            
+                  revelar.reveal('#efeito-sobe2',
+                {
+                    duration: 500,
+                    distance: '90px',
+                    delay: 600,
+                    once: true,
+                    reset: false
+                })
+            
